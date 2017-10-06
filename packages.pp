242738bd1faf87e726e3ef6a97506b3ff370c6cc
Unknown <pomy_jamie@hotmail.com>
2017-10-06 21:47:22 +1100

    Simplification
    Packages now simplified to save space and effectiveness

--- packages.pp
+++ packages.pp
@@ -1,82 +1,17 @@
 # Ensures that all packages specified are installed and installed
 class packages{
-	package { 'openssh' :
+	$services = [
+		'openssh', 'httpd', 'mysql-server', 'mysql', 
+		'vncserver', 'tmux', 'dia2code', 'lynx', 'gcc',
+		'gdb', 'cgdb', 'vim', 'emacs', 'sshfs', 
+		'puppet-lint', 'csh',
+	]
+
+	package { $services :
 		ensure => 'installed',
-		enable => true,
 	}
 
-	package { 'httpd' :
-		ensure => 'installed',
-		enable => true,
-	}
-
-	package { 'mysql-server' :
-		ensure => 'installed',
-		enable => true,
-	}
-
-	package { 'mysql' :
-		ensure => 'installed', 
-		enable => true,
-	}
-
-	package { 'vncserver':
-		ensure => 'installed', 
-		enable => true,
-	}
-
-	package { 'tmux' : 
-		ensure => 'installed', 
-		enable => true,
-	}
-
-	package { 'dia2code' : 
-		ensure => 'installed', 
-		enable => true,
-	}
-
-	package { 'lynx' : 
-		ensure => 'installed', 
-		enable => true,
-	}
-
-	package { 'gcc' : 
-		ensure => 'installed', 
-		enable => true,
-	}
-
-	package { 'gdb' : 
-		ensure => 'installed', 
-		enable => true,
-	}
-
-	package { 'cgdb' : 
-		ensure => 'installed', 
-		enable => true,
-	}
-
-	package { 'vim' : 
-		ensure => 'installed', 
-		enable => true,
-	}
-
-	package { 'emacs' : 		
-		ensure => 'installed', 
-		enable => true,
-	}
-
-	package { 'sshfs' :
-		ensure => 'installed', 
-		enable => true,
-	}
-
-	package { 'puppet-lint' :
-		ensure 		=> '1.1.0',
-		provider 	=> 'gem',
-	}
-
-	package { 'csh' :
-		ensure => 'installed', 
+	service { $services :
 		enable => true,
 	}
 }
