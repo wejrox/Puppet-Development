47612bbb5d21ba37e777933a49971ae26d0a2520
Unknown <pomy_jamie@hotmail.com>
2017-10-06 23:22:20 +1100

    Changed package names
    Package names have been updated for testing with Yum

--- packages.pp
+++ packages.pp
@@ -1,15 +1,24 @@
 # Ensures that all packages specified are installed and installed
 class packages{
+	exec { 'getDeps' :
+		command => "yum-config-manager --enable rhui-REGION-rhel-server-optional",
+		onlyif 	=> "service --status-all | grep -Fq 'lynx'",
+	}
+
+	exec {'getDia2Code' :
+		source => 'http://sourceforge.net/projects/dia2code/files/dia2code/0.8.3-1.x86_64.rpm',
+	}
+
 	$services = [
-		'openssh', 'httpd', 'mysql-server', 'mysql-client', 
-		'vnc4server', 'tmux', 'dia2code', 'lynx', 'gcc',
-		'gdb', 'cgdb', 'vim', 'emacs', 'sshfs', 
+		'openssh', 'httpd', 'mysql-server', 'mysql', 
+		'tigervnc-server', 'tmux', 'lynx', 'gcc',
+		'gdb', 'cgdb', 'vim', 'emacs', 'fuse-sshfs', 
 		'puppet-lint', 'csh',
 	]
 
 	package { $services :
-		ensure => 'installed',
-		provider => 'apt',
+		ensure 		=> 'installed',
+		provider 	=> 'yum',
 	}
 
 	service { $services :
