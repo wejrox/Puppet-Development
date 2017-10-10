f0367a76903dbc505cad488fafe503d29c14665a
Unknown <pomy_jamie@hotmail.com>
2017-10-10 16:25:48 +1100

    Updated packages
    Attempting specifying RPM to handle https download.

--- packages.pp
+++ packages.pp
@@ -24,7 +24,7 @@ class packages{
 	package { 'dia2Code' :
 		ensure 	=> 'installed',
 		source 	=> 'https://downloads.sourceforge.net/project/dia2code/dia2code/0.8.3/dia2code-0.8.3-3.1.i586.rpm?r=http%3A%2F%2Fdia2code.sourceforge.net%2Fdownload.html&ts=1507292790&use_mirror=ncu',
-		name	=> 'dia2Code',
+		provider => 'rpm',
 	}
 
 	$services = [
