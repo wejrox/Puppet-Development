c878eff132a4040116050e3431f29dd16b7255a3
Unknown <pomy_jamie@hotmail.com>
2017-10-10 14:10:20 +1100

    Added MOTD, /usr/local/bin, deps
    Message of the day now informs when client is accessed.
    /usr/local/bin now included in user file path.
    Dependencies have been updated for packages.

--- iniConfig.pp
+++ iniConfig.pp
@@ -3,13 +3,23 @@ class iniConfig {
 		context => "/etc/puppetlabs/puppet/puppet.conf/agent",
 		changes => [
 			"set runinterval 20m",
-			],
+		],
 	}
 
 	augeas { 'runlevel': 
 		context => "/files/etc/inittab",
 		changes => [
 			"set id/runlevels 3",
-			],
-		}
+		],
+	}
+
+	# Set the timestamp to client
+	$timeStamp = generate('/bin/date', '+%d-%m-%Y_%H.%M.%S')
+	notice("Agent started running at $timeStamp")
+
+	# Include /usr/local/bin to user
+	file {'/etc/profile.d/set-user-bin.sh':
+		mode 	=> '644',
+		content => 'PATH=$PATH:/usr/local/bin',
+	}
 }
--- packages.pp
+++ packages.pp
@@ -6,15 +6,18 @@ class packages{
 	}
 
 	exec { 'getDeps' :
-		command => 'yum-config-manager --enable rhui-REGION-rhel-server-optional',
+		onlyif	=> '/usr/bin/yum-config-manager',
+		command => '/usr/bin/yum-config-manager --enable rhui-REGION-rhel-server-optional',
 	}
 
 	exec { 'getDia2Code' :
-		command => 'wget ttps://downloads.sourceforge.net/project/dia2code/dia2code/0.8.3/dia2code-0.8.3-3.1.i586.rpm?r=http%3A%2F%2Fdia2code.sourceforge.net%2Fdownload.html&ts=1507292790&use_mirror=ncu',
+		onlyif	=> '/usr/bin/test -x /usr/bin/wget',
+		command => '/usr/bin/wget https://downloads.sourceforge.net/project/dia2code/dia2code/0.8.3/dia2code-0.8.3-3.1.i586.rpm?r=http%3A%2F%2Fdia2code.sourceforge.net%2Fdownload.html&ts=1507292790&use_mirror=ncu',
 	}
 
 	exec { 'installDia2Code' :
-		command => 'yum install dia2code-0.8.3-3.1.i586.rpm',
+		onlyif 	=> '/usr/bin/test -x /usr/bin/yum',
+		command => '/usr/bin/yum install dia2code-0.8.3-3.1.i586.rpm',
 	}
 
 	$services = [
