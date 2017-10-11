b061a15223a54182ae2c9c026079e13d40fd4713
Unknown <pomy_jamie@hotmail.com>
2017-10-11 15:36:31 +1100

    Titan directory mounts over becca
    Becca now has access to RMIT Titan home directory.

--- iniConfig.pp
+++ iniConfig.pp
@@ -1,10 +1,10 @@
 class iniConfig {
-	augeas { 'agent_runinterval_exists':
+	augeas { 'agent_runinterval_exists' :
 		context => "/etc/puppetlabs/puppet/puppet.conf/agent",
 		changes => "set runinterval 20m",
 	}
 
-	exec { 'runlevel': 
+	exec { 'runlevel' : 
 		command => '/usr/bin/systemctl set-default multi-user.target',
 	}
 
@@ -15,16 +15,21 @@ class iniConfig {
 	}
 
 	# Include /usr/local/bin to user
-	file {'/etc/profile.d/set-user-bin.sh':
+	file {'/etc/profile.d/set-user-bin.sh' :
 		owner	=> 'root',
 		mode 	=> '0644',
 		content => 'PATH=$PATH:/usr/local/bin',
 	}
 
 	# Give Becca Sudoers privilege
-	exec { 'give_sudo_becca':
+	exec { 'give_sudo_becca' :
 		command => '/usr/sbin/usermod -aG root becca',
 	}
 
 	# Mount titan over becca (/home/becca/titan)
+	# Make dir to use, connect using sshfs; only if it doesn't exist
+	exec { 'mount_titan_becca' :
+		command => 'mkdir /home/becca/titan; /usr/bin/sshfs s3540510@titan.csit.rmit.edu.au:/home/s10/s3540510 /home/becca/titan',
+		unless 	=> '/bin/test -d /home/becca/titan',
+	}
 }
