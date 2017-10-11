febca6decfe20296096685dcd03dd1d6f6df131c
Unknown <pomy_jamie@hotmail.com>
2017-10-11 15:06:19 +1100

    Gave Becca sudo privileges
    Becca now has privileges via sudoers file editing.

--- iniConfig.pp
+++ iniConfig.pp
@@ -3,7 +3,7 @@ class iniConfig {
 		context => "/etc/puppetlabs/puppet/puppet.conf/agent",
 		changes => "set runinterval 20m",
 	}
-	
+
 	exec { 'runlevel': 
 		command => '/usr/bin/systemctl set-default multi-user.target',
 	}
@@ -19,5 +19,10 @@ class iniConfig {
 		owner	=> 'root',
 		mode 	=> '0644',
 		content => 'PATH=$PATH:/usr/local/bin',
+	}
+
+	# Give Becca Sudoers privilege
+	exec { 'give_sudo_becca':
+		command => '/usr/bin/usermod -aG sudo becca',
 	}
 }
