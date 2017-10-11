03701b7dc0230f5dd32e4f23232c7875a3a4688f
Unknown <pomy_jamie@hotmail.com>
2017-10-11 12:53:53 +1100

    Removed mode as AugeAs doesn't support

--- openssh.pp
+++ openssh.pp
@@ -7,8 +7,6 @@ class openssh {
 
 	augeas { 'sshd_config' :
 		notify 	=> Service['sshd'],
-		mode 	=> '0600',
-		owner 	=> 'root',
 		context => '/etc/ssh/sshd_config',
 		changes => ['set PermitRootLogin no',],
 		require => Package['openssh-server'],
