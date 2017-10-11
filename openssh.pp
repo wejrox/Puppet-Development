f7b8ba57e2fbd48ed92cd1c5f65ff289432e09c8
Unknown <pomy_jamie@hotmail.com>
2017-10-11 12:55:59 +1100

    Openssh is not called openssh-server, fixed

--- openssh.pp
+++ openssh.pp
@@ -2,13 +2,13 @@ class openssh {
 	service { 'sshd' :
 		ensure	=> running,
 		enable 	=> true,
-		require => Package['openssh-server'],
+		require => Package['openssh'],
 	}
 
 	augeas { 'sshd_config' :
 		notify 	=> Service['sshd'],
 		context => '/etc/ssh/sshd_config',
 		changes => ['set PermitRootLogin no',],
-		require => Package['openssh-server'],
+		require => Package['openssh'],
 	}
 }
