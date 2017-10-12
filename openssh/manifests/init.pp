bdfe85479c9cc9ecedb3452d9ec9cf2c4226f01f
Unknown <pomy_jamie@hotmail.com>
2017-10-13 09:28:49 +1100

    Updated file subscription
    Subscription was looping

--- openssh/manifests/init.pp
+++ openssh/manifests/init.pp
@@ -1,20 +1,18 @@
 # Ensures ssh is running and that root access is disabled
 class openssh {
-  service { 'sshd' :
-    ensure  => running,
-    enable  => true,
-    require => Package['openssh-server'],
-  }
-
   # Overwrite sshd_config with custom one. 
   # Augeas cannot handle commenting.
-  ~> file { '/etc/ssh/sshd_config':
+  file { '/etc/ssh/sshd_config':
     ensure  => present,
-    notify  => Service['sshd'],
     mode    => '0777',
     owner   => 'root',
     group   => 'root',
     source  => '/etc/puppetlabs/code/environments/production/manifests/configfiles/sshd_config',
+    require => Package['openssh-server'],
+  }
+  ~> service { 'sshd' :
+    ensure  => running,
+    enable  => true,
     require => Package['openssh-server'],
   }
 }
--- services/manifests/init.pp
+++ services/manifests/init.pp
@@ -3,21 +3,20 @@ class services {
   file {'/var/www/s3540510':
     ensure => 'directory',
     owner  => 'root',
-    mode   => '0600',
+    mode   => '0644',
   }
-  -> service { 'httpd' :
-    ensure => running,
-    enable => true,
-  }
-
   # Overwrite httpd.conf with custom configuration
-  ~> file { '/etc/httpd/conf/httpd.conf' :
+  -> file { '/etc/httpd/conf/httpd.conf' :
     ensure  => present,
-    notify  => Service['httpd'],
-    mode    => '0777',
+    mode    => '0644',
     owner   => 'root',
     group   => 'root',
     source  => '/etc/puppetlabs/code/environments/production/manifests/configfiles/httpd.conf',
     require => Package['httpd'],
+  }
+  # Subscribe to service (restart on file change)
+  ~> service { 'httpd' :
+    ensure => running,
+    enable => true,
   }
 }
